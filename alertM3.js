851babd59879ebbb749bdf2f8ae28a45e1e7e9b6
D. Baptista <117957767+D-Baptista@users.noreply.github.com>
2025-08-11 00:56:53 +0100

    Delete alertM3.js

--- alertM3.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.nav-link.alerta').forEach(function(link) {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      alert('This feacture is not avaliable yet');
-    });
-  });
-});                                                                 
